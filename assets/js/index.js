a0e0766e3da5632638c479f69cce1ff4e38094f3
dsilva <sfadiego@gmail.com>
2024-08-01 14:51:51 -0600

    actualizando portafolio

---
+++ assets/js/index.js
@@ -0,0 +1,86 @@
+const experience = [
+    {
+        period: `Hoy`,
+        name: "Axolote",
+        miRol: 'Mi rol es el desarrollo de apis para los modulos asi como el mantenimiento a apis existentes.',
+        description: `Empresa colimense encargada de ofrecer sistemas y paginas web a la medida,`,
+        languages: [
+            'Laravel', 'Javascript', 'Mysql', 'ReactJs'
+        ],
+        links: [
+            {
+                web: "https://axolotesource.io/",
+                facebook: "https://www.facebook.com/AxoloteSource"
+            }
+        ],
+    },
+    {
+        period: `2021 a la Fecha`,
+        name: "Now-Tech",
+        miRol: 'Mi trabajo es desarrollo de nuevos modulos, resolucion de bugs y mantenimiento de plataformas',
+        description: `Empresa dedicada a ofrecer soluciones para automatizar procesos de ventas definidos y repetitivos, 
+            Ofrece cursos de capacitaciones para distribuidores y vendedores.`,
+        languages: [
+            'Laravel', 'Javascript', 'Mysql'
+        ],
+        links: [{
+            web: "https://now-tech.com/"
+        }],
+    },
+    {
+        period: `2020 - 2021`,
+        name: "Shohex",
+        miRol: 'Mi rol es el desarrollo de nuevos modulos, Apis, y mantenimiento de sistemas existentes ',
+        languages: [
+            'Laravel', 'Javascript', 'Mysql'
+        ],
+        description: `Empresa que ofrece soluciones a procesos lineales, su principal cliente son las agencias de Ford en Jalisco, y Queretaro`,
+        links: [
+            {
+                web: "https://sohex.mx/",
+                facebook: "https://www.facebook.com/SohexMX/"
+            }
+        ]
+    },
+    {
+        period: `2019 - 2020`,
+        name: "Creatools",
+        miRol: "Tuve la oportunidad de participar como desarrollador Fullstack, donde estuve a cargo de la creación de páginas web y del mantenimiento de sistemas ya existentes.",
+        languages: [
+            'Laravel', 'Javascript', 'ReactJs', 'Mysql'
+        ],
+        description: `startup colimense enfocada al desarrollo y consultoria.`,
+        links: [
+            {
+                facebook: "https://www.facebook.com/CreatoolsColima/"
+            }
+        ]
+    },
+    {
+        period: `2016 a 2020`,
+        name: "Gobierno del Estado de Colima",
+        miRol: `Desempeñé el papel de desarrollador Fullstack en el Complejo Administrativo, donde estuve a cargo de la creación y el mantenimiento de sitios web para diversas dependencias gubernamentales.`,
+        description: `Icsic es la dependencia gubernamental que se encarga de la creación y el mantenimiento de sitios web y sistemas administrativos para dependencias estatales.`,
+        languages: [
+            'PHP', 'Javascript', 'Mysql'
+        ],
+        links: [
+            {
+                web: "https://www.col.gob.mx/"
+            }
+        ],
+    },
+    {
+        period: `2014 a 2016`,
+        name: "Trainee",
+        miRol: `Inicié mi carrera como desarrollador en una variedad de empresas y organismos gubernamentales, lo cual me proporcionó la experiencia esencial para impulsar mi desarrollo profesional.`,
+        languages: [
+            'PHP', 'Laravel', 'Javascript', 'Mysql'
+        ],
+        description: `Supremo Tribunal de Justicia de Colima, 4Tools, Agencia de mazda`,
+        links: []
+    },
+];
+
+
+console.log(experience);
